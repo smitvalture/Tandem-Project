b3fab28ab215c333093f726c075f13b285406fac
Smit Valture <valture16@gmail.com>
2023-02-20 16:59:28 +0530

    landing

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,17 @@
 module.exports = {
   content: ["*"],
   theme: {
+    screens: {
+      'tablet': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'laptop': '1160px',
+      // => @media (min-width: 1024px) { ... }
+
+      'desktop': '1280px',
+      // => @media (min-width: 1280px) { ... }
+    },
+
     extend: {
       fontFamily: {
         'Poppins': ['sans-serif'],
